4dd11bd63c7576bb976b5ae015add0bea00c18f8
Aditya Chaurasia <1212aditya11@gmail.com>
2024-01-21 00:26:23 +0530

    landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Create from "./Pages/Create";
 import Temp from "./Pages/Temp";
-import { MyProvider } from "./api/NameContext.jsx";
 import Navbar from "./Components/Navbar.jsx";
 
 function App() {
   return (
-    <MyProvider>
-      <BrowserRouter>
-      <Navbar/>
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/temp" element={<Temp />} />
-        </Routes>
-      </BrowserRouter>
-    </MyProvider>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/temp" element={<Temp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
